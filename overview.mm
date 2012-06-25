7ca5a260f47fc4bba2817e8a79fe3229f1c98164
Jaroslav Vazny <jaroslav.vazny@gmail.com>
2012-06-25 15:02:03 +0000

    changed overview

---
+++ overview.mm
@@ -0,0 +1,36 @@
+<map version="1.0.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1340572274018" ID="ID_1335669534" MODIFIED="1340574345488" STYLE="bubble">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#160;&#160;&#160;&#160;&#160;&#160;Practical
+    </p>
+    <p>
+      Astroinformatics
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge STYLE="sharp_bezier"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1340572628469" ID="ID_624369313" MODIFIED="1340624549783" POSITION="right" TEXT="Data Aquisition">
+<node CREATED="1340573459940" ID="ID_49130314" MODIFIED="1340573473084" TEXT="Sload Digital Sky Survey"/>
+<node CREATED="1340573474137" ID="ID_1694029729" MODIFIED="1340573481928" TEXT="Virtual Observatory"/>
+</node>
+<node CREATED="1340572673028" ID="ID_495952166" MODIFIED="1340573397288" POSITION="right" TEXT="Toolbox">
+<node CREATED="1340636317179" ID="ID_1666780635" MODIFIED="1340636324222" TEXT="Command Line"/>
+<node CREATED="1340573509957" ID="ID_1072830341" MODIFIED="1340573524644" TEXT="Revision Control"/>
+<node CREATED="1340573525463" ID="ID_452972795" MODIFIED="1340573529476" TEXT="Python"/>
+<node CREATED="1340573538589" ID="ID_1495831877" MODIFIED="1340573543941" TEXT="Topcat"/>
+</node>
+<node CREATED="1340572684166" ID="ID_1599131249" MODIFIED="1340573397290" POSITION="right" TEXT="Data Analysis">
+<node CREATED="1340573544751" ID="ID_367701615" MODIFIED="1340573548372" TEXT="Pandas"/>
+</node>
+<node CREATED="1340572709531" ID="ID_485733717" MODIFIED="1340573397291" POSITION="left" TEXT="Motivation"/>
+<node CREATED="1340572727869" ID="ID_1701410376" MODIFIED="1340573397293" POSITION="left" TEXT="Principles"/>
+</node>
+</map>
